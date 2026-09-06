98befdee897c7c32fe4d2ad84f913da917964a70
guybarnahum <guybarnahum@users.noreply.github.com>
2026-09-05 20:59:23 -0700

    Load Slice A pane polish

--- public/node-hover.js
+++ public/node-hover.js
@@ -176,6 +176,7 @@
 
         appendScript('/person-pane.js', 'data-family-person-pane', build);
         appendScript('/person-pane-position.js', 'data-family-person-pane-position', build);
+        appendScript('/slice-a-polish.js', 'data-family-slice-a-polish', build);
         appendScript('/print-polish.js', 'data-family-print-polish', build);
         appendScript('/print-refinement.js', 'data-family-print', build);
         appendScript('/planar-core.js', 'data-family-planar-core', build);
